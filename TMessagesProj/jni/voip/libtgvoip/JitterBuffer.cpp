d4a35253662ff76d5cd382f05497b595420a55bd
Yasue106601 <Ysu106601@gmail.com>
2026-07-26 15:41:32 +0300

    Apply fast adaptive jitter average 10

--- TMessagesProj/jni/voip/libtgvoip/JitterBuffer.cpp
+++ TMessagesProj/jni/voip/libtgvoip/JitterBuffer.cpp
@@ -368,7 +368,7 @@ void JitterBuffer::Tick(){
 	}
 
 	delayHistory.Add(GetCurrentDelay());
-	avgDelay=delayHistory.Average(12);
+	avgDelay=delayHistory.Average(10);
 
 	double stddev=0;
 	double avgdev=deviationHistory.Average();
