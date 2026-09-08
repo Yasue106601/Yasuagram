942deed9d917bac3a5bca7b1989102a2de60e0fb
Yasue106601 <Ysu106601@gmail.com>
2026-09-09 01:37:41 +0300

    Optimize group call audio latency

--- TMessagesProj/src/main/java/org/webrtc/voiceengine/WebRtcAudioTrack.java
+++ TMessagesProj/src/main/java/org/webrtc/voiceengine/WebRtcAudioTrack.java
@@ -537,7 +537,9 @@ public class WebRtcAudioTrack {
               new AudioAttributes.Builder()
                   .setUsage(usageAttribute)
                   .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                  .setFlags(AudioAttributes.FLAG_LOW_LATENCY)
+                  .setFlags(
+                      AudioAttributes.FLAG_LOW_LATENCY
+                          | AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
                   .build())
           .setAudioFormat(
               new AudioFormat.Builder()
@@ -545,7 +547,7 @@ public class WebRtcAudioTrack {
                   .setSampleRate(sampleRateInHz)
                   .setChannelMask(channelConfig)
                   .build())
-          .setBufferSizeInBytes(bufferSizeInBytes)
+          .setBufferSizeInBytes(Math.max(960, Math.min(bufferSizeInBytes, sampleRateInHz / 100 * 2)))
           .setTransferMode(AudioTrack.MODE_STREAM)
           .setPerformanceMode(AudioTrack.PERFORMANCE_MODE_LOW_LATENCY)
           .build();
