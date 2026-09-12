eed62b01a6543726cbff0aa059734ff954618052
Yasue106601 <Ysu106601@gmail.com>
2026-09-12 20:47:33 +0300

    Test OpenSL ES audio backend

--- TMessagesProj/jni/voip/webrtc/modules/audio_device/audio_device_impl.cc
+++ TMessagesProj/jni/voip/webrtc/modules/audio_device/audio_device_impl.cc
@@ -197,7 +197,7 @@ int32_t AudioDeviceModuleImpl::CreatePlatformSpecificObjects() {
 
 // Real (non-dummy) ADM implementations.
 #else
-  AudioLayer audio_layer(PlatformAudioLayer());
+  AudioLayer audio_layer = kAndroidOpenSLESAudio;
 // Windows ADM implementation.
 #if defined(WEBRTC_WINDOWS_CORE_AUDIO_BUILD)
   if ((audio_layer == kWindowsCoreAudio) ||
