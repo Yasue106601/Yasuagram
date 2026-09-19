b9d462ef25512dc7ffd06903793b18eab20fc52a
Yasue106601 <Ysu106601@gmail.com>
2026-09-20 00:31:28 +0300

    Fix NetEq audio loss from forced preemptive expand

--- TMessagesProj/jni/voip/webrtc/modules/audio_coding/neteq/decision_logic.cc
+++ TMessagesProj/jni/voip/webrtc/modules/audio_coding/neteq/decision_logic.cc
@@ -386,9 +386,8 @@ NetEq::Operation DecisionLogic::ExpectedPacketAvailable(
         if (sync_buffer_ms >= yasu_accelerate_limit_ms) {
           return NetEq::Operation::kAccelerate;
         }
-        if (sync_buffer_ms < low_limit && playout_delay_ms < low_limit) {
-          return NetEq::Operation::kPreemptiveExpand;
-        }
+        // YASU: Do not force preemptive expansion when the buffer is below
+        // target. Keep normal decoding for incoming speech.
       }
     } else {
       const int target_level_samples = TargetLevelMs() * sample_rate_khz_;
